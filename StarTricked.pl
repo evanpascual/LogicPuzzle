770c23d61939f9888587aa41ff9d4bc5893777ef
Evan Pascual <evanpascual@evans-imac.home>
2020-12-02 11:01:30 -0800

    get started on rules

--- StarTricked.pl
+++ StarTricked.pl
@@ -1,14 +1,14 @@
 % Logic Puzzle
 
-day(sunday).
-day(monday).
-day(tuesday).
-day(wednesday).
-day(thursday).
-day(friday).
-day(saturday).
+day(1). %sunday
+day(2).	%monday
+day(3).	%tuesday
+day(4).	%wednesday
+day(5).	%thursday
+day(6).	%friday
+day(7).	%saturday
 
-enthusiast(berrada).
+enthusiast(barrada).
 enthusiast(gort).
 enthusiast(klatu).
 enthusiast(nikto).
@@ -18,22 +18,33 @@ ufo(clothesline).
 ufo(fisbee).
 ufo(watertower).
 
-sighting(D, E, U) :- day(D), enthusiast(E), ufo(U).
 
+
+
+sighting(D, E, U) :- day(D), enthusiast(E), ufo(U). 
+
+%Mr. Klatu made his sighting at some point earlier 
+%in the week than the one who saw the balloon, but 
+%at some point later in the week that the one who 
+%spotted the frisbee (who isnt Ms. Gort)
+
+
+%Fridays sighting was made by either Ms. Barrada
+%or the one who saw a clothesline (or both)
+sighting(D, E, U) :- D=6, E=barrada, U=_.
+sighting(D, E, U) :- D=6, E=_, U=clothesline.
+sighting(D, E, U) :- D=6, E=barrada, U=clothesline.
+
+%Mr. Nikito did not make his sighting on tuesday
+sighting(D, E, U) :- E=nikito, D\=3, U=_.
+
+%Mr. Klatu isnt the one whose object turned out 
+%to be a water tower
+sighting(D, E, U) :- E=klatu, U\=watertower, D=_.
+
+
+
+
+
+%PLACE EVERYTHING IN A SOLVE BLOCK LATER TO SOLVE AND PRINT RESULTS
 %solve :-
-    
-    %Mr. Klatu isnt the one whose object turned out 
-    %to be a water tower
-    
-    
-    %Mr. Nikito did not make his sighting on tuesday
-    
-    
-	%Fridays sighting was made by either Ms. Barrada
-    %or the one who saw a clothesline (or both)
-    
-    
-    %Mr. Klatu made his sighting at some point earlier 
-    %in the week than the one who saw the balloon, but 
-    %at some point later in the week that the one who 
-    %spotted the frisbee (who isnt Ms. Gort)
